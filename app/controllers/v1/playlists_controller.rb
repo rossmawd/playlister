db66b4c4c7b4167bae3bc79c97296eac06af0126
Ross Mawdsley <rossmawd@gmail.com>
2019-08-05 16:12:16 +0100

    + create, destroy & strong params to play contrllr

--- app/controllers/v1/playlists_controller.rb
+++ app/controllers/v1/playlists_controller.rb
@@ -5,4 +5,26 @@ class V1::PlaylistsController < ApplicationController
     render json: @playlists, status: :ok
   end
 
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    @playlist.save
+    render json: @playlist, status: :created
+    # status 201: creted
+  end
+
+  def destroy
+    @playlist = Playlist.where(id: params[:id]).first
+    if @contact.destroy
+      head[:ok]
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:contact).permit(:name, :description)
+  end
 end
